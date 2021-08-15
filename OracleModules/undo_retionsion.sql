94f847d5bb3d9bc60793436c05c94df62ef033e7
PatrickYoo <yhy1045@naver.com>
2021-08-16 01:15:05 +0900

    modules

--- OracleModules/undo_retionsion.sql
+++ OracleModules/undo_retionsion.sql
@@ -1,16 +1,22 @@
 SHOW
-PARAMETER UNDO_RETENTION; -- �ֱ�
+PARAMETER UNDO_RETENTION;
+-- UNDO_RETENTION 은 최근 15분동안 진행된 commit delete 등의 xecute 정보를 담고 있음
 
--- ������ Ŀ���� �ڷ� ������� ORACLE SQL
+--  ORACLE SQL
 SELECT *
-FROM [������ ������ TABLE �̸�]
-    AS OF TIMESTAMP (SYSTIMESTAMP- INTERVAL '15' MINUTE)
-WHERE [������ �����͸� �ҷ����� ���ǹ� �ڸ�];
+FROM [복구가 필요한 TABLE 이름]
+AS OF TIMESTAMP
+(SYSTIMESTAMP- INTERVAL '15' MINUTE)
+WHERE [DELETE 조건을 걸어 놓았던 조건문 삽입 /*WHERE USER_NAME = '이영현'*/];
+/* WHERE USER_NAME = '이영현' */
 
 
--- ������ Ŀ���� �ڷ� ������� ORACLE SQL
-INSERT INTO [���������� ���̺��]
+
+-- 삭제된 데이터에 대한 INSERT 진행 ORACLE SQL
+INSERT INTO [복구하고자 하는 테이블 이름 입력]
 SELECT *
-FROM [������ ������ TABLE �̸�]
-    AS OF TIMESTAMP (SYSTIMESTAMP- INTERVAL '15' MINUTE)
-WHERE [������ �����͸� �ҷ����� ���ǹ� �ڸ�];
+FROM [삭제가 진행되었던 테이블을 입력함]
+AS OF TIMESTAMP
+(SYSTIMESTAMP- INTERVAL '15' MINUTE)
+WHERE [DELETE 조건을 걸어 놓았던 조건문 삽입 /*WHERE USER_NAME = '이영현'*/];
+
---
+++ README.md
@@ -0,0 +1,15 @@
+# markdownImage
+1. README.md 에 사용될 image 모음
+
+# OracleModules
+1. 유용한 쿼리문 정리
+
+# Quartz
+1. java SpringBoot 쿼츠 기본 코드 작성
+
+# regexp
+1. java 정규표현식 정리 (작성중)
+
+# springbootIbatisprojectModule
+1. 아이바티스 기본 셋팅과 로그인 로직이 구현된 Starter 모듈
+
